1640bb0f14cccb69af7f5aad0faaaa98711546d8
DeanvanZwietering <302471757@student.rocmondriaan.nl>
2022-02-04 15:37:27 +0100

    created view pages for categories and products along with corresponding routes

--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,5 +26,29 @@ class BezoekerController extends AbstractController
     public function indexAction()
     {
         return $this->render("bezoeker/index.html.twig");
+    }
+
+    /**
+     * @Route("/categorieen", name="categories")
+     */
+    public function readCategories(CategoryRepository $categoryRepository)
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render("bezoeker/categories.html.twig", [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("categorie/{category_id}", name="category_products")
+     */
+    public function readProductsByCategory($category_id, ProductRepository $productRepository)
+    {
+        $products = $productRepository->findBy(['category_id' => $category_id]);
+
+        return $this->render("bezoeker/products.html.twig", [
+            'products' => $products,
+        ]);
     }
 }
